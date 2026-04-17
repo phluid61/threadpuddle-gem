49ab04dba5d47d6e5955c0d169790be2115766cf
Matthew Kerwin <matthew.kerwin@qut.edu.au>
2026-04-17 14:31:38 +1000

    make CI skip all tests that will never work on an engine

--- test/test_threadpuddle_forking.rb
+++ test/test_threadpuddle_forking.rb
@@ -5,7 +5,7 @@ require "#{File.dirname File.dirname(__FILE__)}/lib/threadpuddle"
 require "#{File.dirname File.dirname(__FILE__)}/lib/forkingthread"
 class Test_threadpuddle_forking < Test::Unit::TestCase
   def setup
-    omit_if RUBY_ENGINE == 'jruby', 'fork is not available on JRuby'
+    omit_unless Process.respond_to?(:fork), 'fork is not available on this platform'
   end
 
   def test_threadpuddle_forking
